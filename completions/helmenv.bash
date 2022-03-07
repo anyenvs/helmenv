06a8e612179c05a6572986e81f33d9c4ede6ae55
Vlad M <vyacheslav.malinovskyy@ibm.comm>
2022-03-07 16:12:47 -0500

    Adding completions and install --list option

---
+++ completions/helmenv.bash
@@ -0,0 +1,16 @@
+_helmenv() {
+  COMPREPLY=()
+  local word="${COMP_WORDS[COMP_CWORD]}"
+
+  if [ "$COMP_CWORD" -eq 1 ]; then
+    COMPREPLY=( $(compgen -W "$(helmenv commands)" -- "$word") )
+  else
+    local words=("${COMP_WORDS[@]}")
+    unset words[0]
+    unset words[$COMP_CWORD]
+    local completions=$(helmenv completions "${words[@]}")
+    COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+  fi
+}
+
+complete -F _helmenv helmenv
